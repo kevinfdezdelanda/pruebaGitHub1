a1c0a7e0d42436ea262a313f31ad03f2f9b67c72
kevinfdezdelanda <61690864+kevinfdezdelanda@users.noreply.github.com>
2020-06-01 13:27:16 +0200

    Add files via upload

--- Junit2/src/ej8/AllTests.java
+++ Junit2/src/ej8/AllTests.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-package ej8;
+
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,16 +9,3 @@ import org.junit.runners.Suite.SuiteClasses;
 public class AllTests {
 
 }
-=======
-package ej8;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ Divide.class, Multiplica.class, Resta.class, Suma.class })
-public class AllTests {
-
-}
->>>>>>> ef0976c4992fd43594b0a9afe1d0a8a22011c2c7
